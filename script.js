0dc2063b86d8017c963d55f0432b46e03a88434c
Robert Litwiński <robert.lit97@gmail.com>
2025-05-04 23:54:41 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://tasksv3.onrender.com/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const trelloApiRoot = 'https://api.trello.com/1';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
