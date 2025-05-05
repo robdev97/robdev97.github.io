df9a6db9c7ff94e2af2b9fc530f7c19a3597ef51
Robert Litwiński <robert.lit97@gmail.com>
2025-05-05 19:16:50 +0200

    test commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://tasksv3.onrender.com/v1/tasks';
-  const trelloApiRoot = 'https://tasksv3.onrender.com/v1/tasks';
+  const trelloApiRoot = 'https://tasksv3.onrender.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
